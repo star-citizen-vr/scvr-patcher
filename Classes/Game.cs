044eb4cf08f563b5a22585b2152cfb5ea4104c50
Bluscream <Bluscream@users.noreply.github.com>
2023-12-12 18:15:51 +0100

    send help pls UwU

--- Classes/Game.cs
+++ Classes/Game.cs
@@ -1,34 +1,69 @@
-﻿using System.IO;
-using System.Windows.Forms;
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using NLog;
+using System.IO;
+using System.Windows;
 
 namespace SCVRPatcher {
     internal class Game {
-        public static readonly List<string> BuildDirectoryNames = new() { "LIVE", "PTU", "EPTU" };
+        public const string Name = "Star Citizen";
+        public const string Author = "Cloud Imperium Games";
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private static readonly List<string> BuildDirectoryNames = new() { "LIVE", "PTU", "EPTU", "HOTFIX", "TECH-Preview" };
 
         public DirectoryInfo BuildRootDirectory { get; private set; }
         public Dictionary<string, DirectoryInfo> BuildDirectories { get; private set; } = new();
 
         public Game() {
-            BuildRootDirectory = RequestGameRootDirFromUser();
+            BuildRootDirectory = GetGameRootDirFromRegistry() ?? RequestGameRootDirFromUser();
+        }
+
+        public DirectoryInfo? GetGameRootDirFromRegistry() {
+            var key = Registry.CurrentUser.OpenSubKey(@"System\GameConfigStore\Children");
+            if (key is null) {
+                Logger.Error($"Unable to open registry key: {key}");
+                return null;
+            }
+            var subKeysNames = key.GetSubKeyNames();
+            foreach (var subKeyName in subKeysNames) {
+                var subKey = key.OpenSubKey(subKeyName);
+                if (subKey is null) continue;
+                var matchedExeFullPath = subKey.GetValue("MatchedExeFullPath") as string;
+                if (matchedExeFullPath == null) continue;
+                var matchedExe = new FileInfo(matchedExeFullPath);
+                if (matchedExe.Name == "StarCitizen.exe") {
+                    Logger.Info($"Found Star Citizen as {subKey.Name}: {matchedExe.FullName}");
+                    var dir = matchedExe.Directory?.Parent?.Parent;
+                    if (dir is null || !dir.Exists) {
+                        Logger.Error($"Star Citizen's root directory does not exist or is invalid: {dir?.FullName}");
+                        continue;
+                    }
+                    return dir;
+                }
+            }
+            Logger.Warn("Could not find Star Citizen's root directory in the registry, please select it manually!");
+            return null;
         }
 
         public DirectoryInfo RequestGameRootDirFromUser(DirectoryInfo? startingDir = null) {
             startingDir ??= new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles));
-            var dialog = new FolderBrowserDialog();
-            dialog.SelectedPath = startingDir.FullName;
-            dialog.Description = "Select the game's root directory";
-            dialog.ShowNewFolderButton = false;
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            dialog.InitialDirectory = startingDir.FullName;
+            var subDirNames = string.Join(", ", BuildDirectoryNames);
+            dialog.Title = $"Select Star Citizen's root directory (Containing {subDirNames})";
             var result = dialog.ShowDialog();
-            if (result != DialogResult.OK) Environment.Exit(0);
-            var dir = new DirectoryInfo(dialog.SelectedPath);
+            if (result != CommonFileDialogResult.Ok) Environment.Exit(0);
+            Logger.Debug($"User selected directory: {dialog.FileName}");
+            var dir = new DirectoryInfo(dialog.FileName);
             if (!dir.Exists) {
-                MessageBox.Show("The selected directory does not exist or is inaccessible, try again!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("The selected directory does not exist or is inaccessible, try again!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return RequestGameRootDirFromUser(dir);
             }
             var subDirs = dir.GetDirectories();
             // if none of the BuildDirectoryNames exist in the selected directory, ask again
             if (!BuildDirectoryNames.Any(x => subDirs.Any())) {
-                MessageBox.Show($"The selected directory does not contain any of [{BuildDirectoryNames}], try again!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"The selected directory does not contain any of [{subDirNames}], try again!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return RequestGameRootDirFromUser(dir);
             }
             BuildRootDirectory = dir;
