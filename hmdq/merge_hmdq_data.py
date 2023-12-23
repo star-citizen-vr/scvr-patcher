fb52ee25b07f85e73a359f2b33833a8c2fd39338
Bluscream <Bluscream@users.noreply.github.com>
2023-12-23 21:24:23 +0100

    Refactor merge_hmdq_data.py to merge dictionaries recursively

--- hmdq/merge_hmdq_data.py
+++ hmdq/merge_hmdq_data.py
@@ -9,12 +9,23 @@ def get_files():
     # return all json files in hmdq directory
     for f in os.listdir("hmdq"):
         if f.endswith('.json'):
-            if "copy" in f:
+            if "copy" in f or f == "merged.json":
                 print(f"skipping file {f}")
                 continue
             print(f"added file {f}")
             files.append("hmdq/" + f)
     return files
+
+def merge_dicts(d1, d2):
+    # merge two dicts recursively but only keep one of the values
+    for k in d2:
+        if k in d1:
+            if isinstance(d1[k], dict) and isinstance(d2[k], dict):
+                merge_dicts(d1[k], d2[k])
+            else:
+                print(f"skipping key {k}")
+        else:
+            d1[k] = d2[k]
 
 def merge_files():
     # merge all json files in hmdq directory
@@ -25,7 +36,10 @@ def merge_files():
             print(f"read file {f}")
     merged = {}
     for i, d in enumerate(dicts):
-        merged.update(d)
+        print(f"dict {i} has {len(d)} keys")
+        print(f"dict {i} has {len(d['oculus'])} oculus keys")
+        print(f"dict {i} has {len(d['openvr'])} openvr keys")
+        merge_dicts(merged, d) # merged.update(d)
         print(f"merged dict {i}")
     output_file = "hmdq/merged.json"
     with open(output_file, 'w') as f:
